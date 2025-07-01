320cb61a463d0cae9f6f748bb2c786e0f2dc37ae
André Feuzer <andre.heydran@gmail.com>
2025-07-01 01:05:26 -0300

    adicionado mekers no alamo, e setado background do body black

--- script/alamo_sea.js
+++ script/alamo_sea.js
@@ -1,6 +1,12 @@
 import { createMarker } from "./marker.js";
 
-const markers = [];
+const markers = [
+  { x: 415, y: 398, marked: false },
+  { x: 420, y: 433, marked: false },
+  { x: 280, y: 427, marked: false },
+  { x: 319, y: 407, marked: false },
+  { x: 514, y: 210, marked: false },
+];
 
 function loadAlamoMarkers() {
   const map = document.getElementById("map-container");
